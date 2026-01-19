346b313eff9f4cf1754b7d2e6d0f2aca365fa7c7
Andre Anjos <andre.dos.anjos@gmail.com>
2026-01-19 19:10:43 +0100

    build: avoid symlinking and use ninja -D variables to affect luamake build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,22 +2,22 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Create symlinks on Linux because gcc/g++/ar are hardcoded for luamake build
+# Override compiler and archiver on a platform specific basis as, by default, the build
+# of luamake is setup to use "cc" and "ar" on Linux.
 # cf.: https://github.com/actboy168/luamake/blob/master/compile/ninja/linux.ninja
+LUAMAKE_NINJA_OPTS=""
 if [[ ${target_platform} =~ .*linux.* ]]; then
     if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-      ln -sf ${BUILD_PREFIX}/bin/${BUILD}-gcc ${BUILD_PREFIX}/bin/gcc
-      ln -sf ${BUILD_PREFIX}/bin/${BUILD}-ar ${BUILD_PREFIX}/bin/ar
+      LUAMAKE_NINJA_OPTS="-Dcc=${BUILD}-gcc -Dar=${BUILD}-ar"
     else
-      ln -sf ${CC} ${BUILD_PREFIX}/bin/gcc
-      ln -sf ${AR} ${BUILD_PREFIX}/bin/ar
+      LUAMAKE_NINJA_OPTS="-Dcc=${CC} -Dar=${AR}"
     fi
 fi
-
 pushd 3rd/luamake
-compile/build.sh notest
+compile/build.sh ${LUAMAKE_NINJA_OPTS} notest
 popd
 
+# Use just built luamake to compile the language server
 3rd/luamake/luamake -cc "$CC" -ar "$AR" -cflags "$CFLAGS" rebuild -notest
 
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
